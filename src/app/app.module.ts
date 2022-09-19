c9a77c49eef24566410b500b37ca872aadf326bc
Georgia Saina <sainas.georgia@gmail.com>
2022-09-19 16:26:17 +0300

    importing formsmodule in app.module.ts creating authentication service creating checkLogin method

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { DataService } from './service/data.service';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule ,routingComponents} from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
@@ -14,11 +14,13 @@ import { LoginComponent } from './login/login.component';
     routingComponents,
     ProductComponent,
     LoginComponent,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [DataService],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../authentication.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  username = 'javainuse'
+  password = ''
+  invalidLogin = false
+
+  constructor(private router: Router,
+    private loginservice: AuthenticationService) { }
 
   ngOnInit(): void {
+  }
+
+  checkLogin(){
+    if(this.loginservice.authenticate(this.username, this.password)){
+      this.router.navigate(['']);
+      this.invalidLogin=false;
+
+    }else{
+      this.invalidLogin = false;
+    }
   }
 
 }
