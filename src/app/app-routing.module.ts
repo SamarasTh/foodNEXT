a682539ae70178841025cee5ea71438d6b9bfad9
a.kapsouros <angeloskaps@gmail.com>
2022-09-21 15:15:30 +0300

    deleted an extra comma which caused error

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -25,7 +25,7 @@ const routes: Routes = [
   { path: 'home' , component: HomeComponent},
   { path: '' , component: HomeComponent},
   { path: '**', component: LoginComponent },
-  { path: 'login', component: LoginComponent },
+  { path: 'login', component: LoginComponent }
 
 ];
 
